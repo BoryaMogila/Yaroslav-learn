d0524cbec4af012c28a32f0315a35b659c7f6393
BM086325 <Borys_Mohyla@epam.com>
2021-05-19 21:00:10 +0300

    for

--- lesson-2/my.js
+++ lesson-2/my.js
@@ -33,3 +33,15 @@ function division(a, b) {
 function calculate (a, b, operation) {
     return operation(a, b)
 }
+
+let i = 0;
+do {
+    i = i + 1
+    console.log(i);
+} while (i < 10) 
+
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+
+for(let i = 0; i < numbers.length; ++i) {
+    console.log(numbers[i])
+};
